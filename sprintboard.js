33fb3c67aaae70307d63908dd8cc86ee3c7b419f
James Bliss <astronouth7303@gmail.com>
2012-05-02 16:54:36 -0400

    Some testing to get a static host going.

--- sprintboard.js
+++ sprintboard.js
@@ -2,10 +2,10 @@
 var app = require('express').createServer()
   , io = require('socket.io').listen(app);
 
-app.listen(80);
+app.listen(8080);
 
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+app.get(/^(\/.*)$/, function (req, res) {
+    res.sendfile(__dirname + '/static/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
